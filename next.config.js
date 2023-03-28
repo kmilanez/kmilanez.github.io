41cd1bcb429864db14aa1da5f04d80e22f44ce7f
Karlos Milanez <karlos@kmilanez.com>
2023-03-28 20:17:19 +0200

    feat(page): initial commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: 'export',
+  basePath: '',
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
