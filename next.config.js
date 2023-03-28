ced2af19bcfd07c81ef6d6001f313792f108e5a9
Karlos Milanez <karlos@kmilanez.com>
2023-03-28 21:31:07 +0200

    fix(config): remove image optimizations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: '',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
